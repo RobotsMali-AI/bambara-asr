1ce2f93e5b1650b025009971686ae24bb6a14fda
diarray-hub <yacoudiarra.wl@gmail.com>
2026-06-03 16:36:38 +0000

    Update Dependency installation script

--- utils/bash/install_dependencies.sh
+++ utils/bash/install_dependencies.sh
@@ -8,21 +8,29 @@
 
 set -e
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <requirements.txt>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <requirements.txt> <venv_path>"
     exit 1
 fi
 
 REQ_FILE="$1"
+VENV_PATH="$2"
 
 if [ ! -f "$REQ_FILE" ]; then
     echo "Error: Requirements file '$REQ_FILE' not found."
     exit 1
 fi
 
-# Install pip if not already installed
+# Install Python 3.10 (ubuntu)
 sudo apt update
-sudo apt install -y python3-pip
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.10 python3.10-venv python3.10-dev -y
+
+# Create a virtual environment
+python3.10 -m venv "$VENV_PATH"
+source "$VENV_PATH/bin/activate"
 
 # OS packages (Ubuntu)
 if ! sudo apt-get install -y libsndfile1 ffmpeg; then
